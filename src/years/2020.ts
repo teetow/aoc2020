63bcb1da71ee0931bf8bace5d67ac3f11aeafa38
Johan Althoff <teetow@gmail.com>
2020-12-03 19:59:55 +0100

    complete day3 2020

--- src/years/2020.ts
+++ src/years/2020.ts
@@ -1,10 +1,12 @@
 import { Day } from "util/Day";
 import day1 from "./2020/day1";
 import day2 from "./2020/day2";
+import day3 from "./2020/day3";
 
 const days = new Map<number, Day<any>>();
 
 days.set(1, { ...day1 });
 days.set(2, { ...day2 });
+days.set(3, { ...day3 });
 
 export default days;
---
+++ src/years/2020/day3.ts
@@ -0,0 +1,54 @@
+import { Day } from "util/Day";
+import { data as day3data, testdata } from "./data/day3";
+
+type Slope = {
+  stepX: number;
+  stepY: number;
+};
+
+const traverseSlope = (data: string[][], { stepX, stepY }: Slope): number => {
+  let trees = 0;
+  let x = 0;
+
+  for (let y = 0; y < data.length; y += stepY) {
+    if (data[y][x] === "#") trees += 1;
+    x = (x + stepX) % data[0].length;
+  }
+  return trees;
+};
+
+const traverseSlopes = (data: string[][], slopes: Slope[]) => {
+  const results = slopes.map((slope) => traverseSlope(data, slope));
+  return results.reduce((a, b) => {
+    return a * b;
+  });
+};
+
+const part2slopes = [
+  { stepX: 1, stepY: 1 },
+  { stepX: 3, stepY: 1 },
+  { stepX: 5, stepY: 1 },
+  { stepX: 7, stepY: 1 },
+  { stepX: 1, stepY: 2 },
+];
+
+const day3: Day<string[][]> = {
+  title: "Day 3",
+  data: day3data,
+  parts: [
+    {
+      title: "Part 1",
+      func: (data) => {
+        return traverseSlope(data, { stepX: 3, stepY: 1 });
+      },
+      tests: [{ data: testdata, result: 7 }],
+    },
+    {
+      title: "Part 2",
+      func: (data) => traverseSlopes(data, part2slopes),
+      tests: [{ data: testdata, result: 2 * 7 * 3 * 4 * 2 }],
+    },
+  ],
+};
+
+export default day3;

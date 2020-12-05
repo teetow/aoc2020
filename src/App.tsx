21f3c49e498217281f013056981d49adfde63f93
Johan Althoff <teetow@gmail.com>
2020-12-05 23:53:55 +0100

    add actual routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,75 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { BrowserRouter, Route, useParams } from "react-router-dom";
 import { Day } from "util/Day";
-
+import "./App.scss";
 import DayPanel from "./components/DayPanel";
 import Picker from "./components/Picker";
+import "./styles.scss";
 import years from "./years/years";
 
-import "./styles.scss";
-import "./App.scss";
-
-const parseLocationPath = (path: string): { year: number; day: number } => {
-  const [year, day] = path.split("/").slice(-2);
-  return { year: Number(year) || 0, day: Number(day) || 0 };
+type RouteParams = {
+  year: string;
+  day: string;
 };
 
-const App: FunctionComponent = () => {
-  const [currentYearIndex, setCurrentYearIndex] = useState(
-    parseLocationPath(window.location.pathname).year ||
-      Number(localStorage.getItem("currentYear") || "")
-  );
+const AppMain = () => {
+  // eslint-disable-next-line
+  const { year, day } = useParams<RouteParams>();
+  const yearIndex = Number(year);
+  const dayIndex = Number(day) || 1;
 
-  const [currentDayIndex, setCurrentDayIndex] = useState(
-    parseLocationPath(window.location.pathname).day ||
-      Number(localStorage.getItem("currentDay") || "")
-  );
+  const currentYear = years.get(yearIndex) as Map<number, Day<unknown>>;
 
-  useEffect(() => {
-    window.history.pushState(
-      null,
-      document.title,
-      `/aoc2020/${currentYearIndex}/${currentDayIndex}`
-    );
-  }, [currentYearIndex, currentDayIndex]);
-
-  useEffect(
-    () => localStorage.setItem("currentDay", currentDayIndex.toString()),
-    [currentDayIndex]
-  );
-
-  useEffect(
-    () => localStorage.setItem("currentYear", currentYearIndex.toString()),
-    [currentYearIndex]
-  );
-
-  const currentYear = years.get(currentYearIndex) as Map<number, Day<unknown>>;
-
-  const currentDay = currentYear?.get(currentDayIndex);
+  const currentDay = currentYear?.get(dayIndex);
 
   return (
-    <div className="my-app">
+    <>
       <div className="my-app__nav">
         <Picker
           {...{
             options: Array.from(years.keys()),
-            onChange: (year) => setCurrentYearIndex(year),
-            current: currentYearIndex,
+            current: yearIndex,
           }}
         />
         {currentYear !== undefined && (
           <Picker
             {...{
               options: Array.from(currentYear.keys()),
-              onChange: (day) => setCurrentDayIndex(day),
-              current: currentDayIndex,
+              current: dayIndex,
+              linkPrefix: `${yearIndex}/`,
             }}
           />
         )}
       </div>
       <div className="my-app__main">
         {currentDay !== undefined && (
-          <DayPanel
-            day={currentDay}
-            year={currentYearIndex}
-            dayIndex={currentDayIndex}
-          />
+          <DayPanel day={currentDay} year={yearIndex} dayIndex={dayIndex} />
         )}
       </div>
-
-      <div className="my-app__boilerplate">
-        <span>
-          Made for{" "}
-          <a href="https://adventofcode.com/2020">Advent of Code 2020</a>
-        </span>
-        <span>
-          Source at <a href="https://github.com/teetow/aoc2020">GitHub</a>
-        </span>
-      </div>
-    </div>
+    </>
   );
 };
+
+const App: FunctionComponent = () => {
+  return (
+    <BrowserRouter basename="/aoc2020">
+      <div className="my-app">
+        <Route exact path="/:year?/:day?">
+          <AppMain />
+        </Route>
+
+        <div className="my-app__boilerplate">
+          <span>
+            Made for{" "}
+            <a href="https://adventofcode.com/2020">Advent of Code 2020</a>
+          </span>
+          <span>
+            Source at <a href="https://github.com/teetow/aoc2020">GitHub</a>
+          </span>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
 export default App;
--- src/components/Picker.tsx
+++ src/components/Picker.tsx
@@ -1,18 +1,19 @@
 import classnames from "classnames";
 import React, { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 
 import "./Picker.scss";
 
 type Props = {
   options: number[];
   current: number;
-  onChange: (index: number) => void;
+  linkPrefix?: string;
 };
 
 const Picker: FunctionComponent<Props> = ({
   options,
   current,
-  onChange,
+  linkPrefix,
 }: Props) => {
   const base = "my-picker";
 
@@ -26,16 +27,9 @@ const Picker: FunctionComponent<Props> = ({
   return (
     <div className={`${base}`}>
       {Array.from(options).map((value) => (
-        <div
-          tabIndex={0}
-          role="button"
-          onKeyDown={() => onChange?.(value)}
-          key={value}
-          className={getDayClasses(value)}
-          onClick={() => onChange?.(value)}
-        >
-          {value}
-        </div>
+        <Link to={`/${linkPrefix || ""}${value}`} key={value}>
+          <div className={getDayClasses(value)}>{value}</div>
+        </Link>
       ))}
     </div>
   );

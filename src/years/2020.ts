f5011420e288efde2d98ef9b1b5a3902bf841c49
Johan Althoff <teetow@gmail.com>
2020-12-05 12:17:19 +0100

    complete day 5

--- src/years/2020.ts
+++ src/years/2020.ts
@@ -3,6 +3,7 @@ import day1 from "./2020/day1";
 import day2 from "./2020/day2";
 import day3 from "./2020/day3";
 import day4 from "./2020/day4";
+import day5 from "./2020/day5";
 
 const days = new Map<number, Day<any>>();
 
@@ -10,5 +11,6 @@ days.set(1, { ...day1 });
 days.set(2, { ...day2 });
 days.set(3, { ...day3 });
 days.set(4, { ...day4 });
+days.set(5, { ...day5 });
 
 export default days;
---
+++ src/years/2020/day5.ts
@@ -0,0 +1,82 @@
+import { Day } from "util/Day";
+import day5data from "./data/day5";
+
+type BoardingPass = {
+  row: number;
+  seat: number;
+};
+
+const substMap = {
+  row: [
+    { char: "F", val: 0 },
+    { char: "B", val: 1 },
+  ],
+  seat: [
+    { char: "L", val: 0 },
+    { char: "R", val: 1 },
+  ],
+};
+
+const decodeRow = (data: string, decoder: keyof typeof substMap): number => {
+  let decodedData = data;
+  substMap[decoder].forEach(({ char, val }) => {
+    decodedData = decodedData.replaceAll(char, val.toString());
+  });
+  return parseInt(decodedData, 2);
+};
+
+const makePass = (data: string) => {
+  const row = data.slice(0, 7);
+  const seat = data.slice(7);
+  return {
+    row: decodeRow(row, "row"),
+    seat: decodeRow(seat, "seat"),
+  };
+};
+
+const makeData = (data: string) => data.split("\n").map((row) => makePass(row));
+
+const calcId = (pass: BoardingPass) => pass.row * 8 + pass.seat;
+
+const getHighestSeatId = (passes: BoardingPass[]) => {
+  const seatIds = passes.map(calcId);
+  return Math.max(...seatIds);
+};
+
+const findSeatIdGap = (passes: BoardingPass[]): number => {
+  const seatIds = passes.map(calcId).sort((a, b) => a - b);
+  return (
+    (seatIds.find((val, index, arr) => val - arr[index - 1] > 1) || -1) - 1
+  );
+};
+
+const day5: Day<BoardingPass[]> = {
+  title: "Day 5 -- Binary Boarding",
+  description: `You dropped your boarding pass! Find your seat through process of elimination.`,
+  data: day5data,
+  dataConv: makeData,
+  parts: [
+    {
+      title: "Part 1",
+      description: `* Find the boarding pass with the highest seat ID`,
+      func: (passes: BoardingPass[]) => getHighestSeatId(passes),
+      tests: [
+        { data: "FBFBBFFRLR", result: 44 * 8 + 5 },
+        { data: "BFFFBBFRRR", result: 70 * 8 + 7 },
+        { data: "FFFBBBFRRR", result: 14 * 8 + 7 },
+        { data: "BBFFBBFRLL", result: 102 * 8 + 4 },
+      ],
+    },
+    {
+      title: "Part 2",
+      description: `* Find the ID of your seat
+* The seats with IDs +1 and -1 from yours will be in your list
+* Some of the seats at the very front and back of the plane don't exist on this aircraft
+* Your seat wasn't at the very front or back
+      `,
+      func: (passes: BoardingPass[]) => findSeatIdGap(passes),
+    },
+  ],
+};
+
+export default day5;

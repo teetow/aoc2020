eebce9153188b7856d07eff76c9a783785078107
Johan Althoff <teetow@gmail.com>
2021-10-16 14:04:18 +0200

    complete day 3 2015

--- src/years/2015/day3.ts
+++ src/years/2015/day3.ts
@@ -5,33 +5,72 @@ const makeData = (data: string) => {
   return data.split("");
 };
 
-type Point = {
-  x: number;
-  y: number;
-};
-const pt = (x: number, y: number): Point => ({ x, y });
+class Pt {
+  x_pos: number;
+  y_pos: number;
+  constructor(x: number, y: number) {
+    this.x_pos = x;
+    this.y_pos = y;
+  }
 
-const Moves: Record<string, Point> = {
-  "<": pt(-1, 0),
-  v: pt(0, 1),
-  ">": pt(1, 0),
-  "^": pt(0, -1),
+  get x() {
+    return this.x_pos;
+  }
+
+  get y() {
+    return this.y_pos;
+  }
+
+  get key() {
+    return Symbol.for(`Pt[${this.x}:${this.y}]`);
+  }
+}
+
+const Moves: Record<string, Pt> = {
+  "<": new Pt(-1, 0),
+  v: new Pt(0, 1),
+  ">": new Pt(1, 0),
+  "^": new Pt(0, -1),
 };
 
-const addPoints = (one: Point, other: Point): Point => {
-  return pt(one.x + other.x, one.y + other.y);
+const sumPoints = (one: Pt, other: Pt): Pt => {
+  return new Pt(one.x + other.x, one.y + other.y);
 };
 
-const getMove = (raw: string) => Moves[raw];
+const makeRun = (
+  map: Map<symbol, number>,
+  run: string[]
+): Map<symbol, number> => {
+  let currentPos = new Pt(0, 0);
+  map.set(currentPos.key, 1);
+  run.forEach((step) => {
+    currentPos = sumPoints(currentPos, Moves[step]);
+    map.set(currentPos.key, (map.get(currentPos.key) || 0) + 1);
+  });
+
+  return map;
+};
 
 const runA = (data: string[]): number => {
-  const map = new Map<Point, number>();
-  const coord = pt(0, 0);
-  map.set(coord, 1);
-  data.forEach((step) => {
-    const newCoord = addPoints(coord, getMove(step));
-    map.set(newCoord, (map.get(newCoord) || 0) + 1);
+  const map = new Map<symbol, number>();
+  makeRun(map, data);
+  return map.size;
+};
+
+const runB = (data: string[]): number => {
+  const map = new Map<symbol, number>();
+  const santaMoves: string[] = [];
+  const roboMoves: string[] = [];
+
+  data.forEach((step, i) => {
+    if (i % 2 === 0) {
+      santaMoves.push(step);
+    } else {
+      roboMoves.push(step);
+    }
   });
+  makeRun(map, santaMoves);
+  makeRun(map, roboMoves);
   return map.size;
 };
 
@@ -47,6 +86,15 @@ const day3: Day<string[]> = {
         { data: ">", result: 2 },
         { data: "^>v<", result: 4 },
         { data: "^v^v^v^v^v", result: 2 },
+      ],
+    },
+    {
+      title: "Part 2",
+      func: runB,
+      tests: [
+        { data: "^v", result: 3 },
+        { data: "^>v<", result: 3 },
+        { data: "^v^v^v^v^v", result: 11 },
       ],
     },
   ],
